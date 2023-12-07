65914ea501bb7622744ea8235fa3c6351b7efebb
karim mohsen <91184437+karimmohsen98@users.noreply.github.com>
2023-12-07 18:35:56 +0100

    added to fucntion so the program wont close after first task completion

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -6,41 +6,51 @@ using namespace std;
 
 int main()
 {
-	double a, b;
-	char operation;
-	cout << "Enter the first number: ";
-	cin >> a;
-	cout << "Enter the second number: ";
-	cin >> b;
-	cout << "Enter the operation: " << "Can be (+,-,/,*,%)";
-	cin >> operation;
-	switch (operation)
+	char choice;
+	do
 	{
-		case
-		'+':
-			cout << a << " + " << b << " = " << a + b;
-			break;
-		case
-		'-':
-			cout << a << " - " << b << " = " << a - b;
-			break;
-		case
-        '*':
-			cout << a << " * " << b << " = " << a * b;
-			break;
-        case
-		'/':
-			cout << a << " / " << b << " = " << a / b;
-			break;
-		case
-		'%':
-			cout << a << " % " << b << " = " << (int)a % (int)b;
-			break;
-		default:
-			cout << "Error! The operator is not correct";
-			break;
 
-	}
+		double a, b;
+		char operation;
+		cout << "Enter the first number: ";
+		cin >> a;
+		cout << "Enter the second number: ";
+		cin >> b;
+		cout << "Enter the operation: " << "Can be (+,-,/,*,%)";
+		cin >> operation;
+		switch (operation)
+		{
+			case
+			'+':
+				cout << a << " + " << b << " = " << a + b;
+				break;
+				case
+				'-':
+					cout << a << " - " << b << " = " << a - b;
+					break;
+					case
+					'*':
+						cout << a << " * " << b << " = " << a * b;
+						break;
+						case
+						'/':
+							cout << a << " / " << b << " = " << a / b;
+							break;
+							case
+							'%':
+								cout << a << " % " << b << " = " << (int)a % (int)b;
+								break;
+							default:
+								cout << "Error! The operator is not correct";
+								break;
+
+		}
+		cout << "\nDo you want to continue? (y/n)";
+		cin >> choice;
+
+	} while (choice == 'Y' || choice == 'y');
+	return 0;
+
 }
 
 
