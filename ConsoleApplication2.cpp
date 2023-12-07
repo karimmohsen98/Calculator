b59ef7fa448e674102ddd24cbe464e3b9c5c31cc
karim mohsen <91184437+karimmohsen98@users.noreply.github.com>
2023-12-07 18:17:37 +0100

    first commit

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,46 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, b;
+	char operation;
+	cout << "Enter the first number: ";
+	cin >> a;
+	cout << "Enter the second number: ";
+	cin >> b;
+	cout << "Enter the operation: " << "Can be (+,-,/,*,%)";
+	cin >> operation;
+	switch (operation)
+	{
+		case
+		'+':
+			cout << a << " + " << b << " = " << a + b;
+			break;
+		case
+		'-':
+			cout << a << " - " << b << " = " << a - b;
+			break;
+		case
+        '*':
+			cout << a << " * " << b << " = " << a * b;
+			break;
+        case
+		'/':
+			cout << a << " / " << b << " = " << a / b;
+			break;
+		case
+		'%':
+			cout << a << " % " << b << " = " << (int)a % (int)b;
+			break;
+		default:
+			cout << "Error! The operator is not correct";
+			break;
+
+	}
+}
+
+
